e9ad14ebb949fcccf02efe386e6e25bacb8e1d70
myname21-meet <myname21@meet.mit.edu>
2019-12-03 12:46:00 -0500

    store

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def store():
 	products= query_all()
 	return render_template("store.html" , products= products)
 
+@app.route("/about")
+def about():
+   return render_template("about.html" )
+
 @app.route('/add_product/')
 def add_the_product():
 	name =request.form["name"]
@@ -24,7 +28,7 @@ def add_the_product():
 	description= request.form["description"]
 	add_product(name, price, picturelink, description)
 	return render_template("store.html")
-
+1
 @app.route('/update_product/')
 def uptade_the_product():
 	name =request.form["name"]
@@ -35,39 +39,22 @@ def uptade_the_product():
 	return render_template("store.html")
 	
 
-@app.route('/delete_product/')
-def delete_the_product(their_price):
-	their_price= request.form["their_price"]
-	delete_product(their_price)
-	return render_template("store.html")
-
-@app.route('/quary_all')
-def quary_all_products():
-	add_to_cart()
-	return render_template("cart.html")
-
-@app.route('/quary_by_name/')
-def quary_by_name_products():
-	their_name=request.form["their_name"]
-	quary_by_name()
-	return render_template("cart.html")
-
-@app.route('/add_to_cart/')
-def add_cart():
-	product_id= request.form["product_id"]
-	product_id=int(product_id)
-	add_to_cart()
-	return render_template("cart.html")
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error= None
 	if request.method == 'POST':
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
 			error = 'Invalid Credentials. Please try again.'
+			return render_template("home.html")
 		else:
-			return redirect(url_for('home'))
-	return render_template('login.html', error=error)
+			return render_template("portal.html")
+	else:
+		return render_template('login.html', error=error)
+
+	
+@app.route('/portal')
+def enter_portal():
+	return render_template ("portal.html")
 
 
 	
